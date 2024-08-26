04430b4d40494a467f78786c3abb53c97f295deb
Chris Norton <contact@chrisnortonjr.com>
2024-08-26 12:39:22 -0400

    add logos

--- src/components/header/MainMenu.jsx
+++ src/components/header/MainMenu.jsx
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import React, {useEffect} from 'react';
 import Image from 'next/image';
-import logo from '@/assets/img/logo.png'
+import logo from '@/assets/img/logo.png';
 
 
 const MainMenu = () => {
