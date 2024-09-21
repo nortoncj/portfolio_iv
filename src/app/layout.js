7948295b765189426df47d7c7cf1b6d34e26d167
Chris Norton <contact@chrisnortonjr.com>
2024-09-20 22:18:49 -0400

    microsoft tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -133,7 +133,9 @@ export default function RootLayout({ children }) {
             gtag("config", "G-S5ZBPZ262W");
         `}
       </Script>
-      <meta name="msvalidate.01" content="60B32BAF819A3A3DD7BD39D7BCDBDA21" />
+      <Head>
+        <meta name="msvalidate.01" content="60B32BAF819A3A3DD7BD39D7BCDBDA21" />
+      </Head>
       <body className={`${manrope.className}`}>
         <ToastContainer />
         <Dependency />
